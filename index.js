c19f79bba1f589238d3f701b608f0b45464a69ad
Yonle <yonle@duck.com>
2022-11-23 16:18:18 +0700

    Remove unnecessary hashtag message being added into post.
    Instead of making it mentions #Indonesia and #Gempa hashtag, Just make it not mentioning any hashtag anymore.
    
    Because user is still able to modify the source code to do it if they want, tho.
    
    Signed-off-by: Yonle <yonle@duck.com>

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ let posts = 0;
 async function post(t, etc) {
   //if (!(posts >= 3)) return posts++;
   return await masto.statuses.create({
-    status: t + "\n#Indonesia #Gempa",
+    status: t,
     visibility: process.env.VISIBILITY || "unlisted",
   });
 }
